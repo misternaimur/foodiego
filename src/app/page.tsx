acf30fd0d9f42c117072a537c4fbd7946f4d2747
Anamul_Haque <anamulhaque0357@gmail.com>
2026-08-19 16:53:44 +0600

    WhatAreYouCraving Section Created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import WhatAreYouCraving from "@/components/WhatAreYouCraving";
+
 export default function Home() {
   return (
     <main className="flex-1">
-      {/* <h1 className="text-2xl font-bold text-center">Hello world...!!!</h1> */}
+      <WhatAreYouCraving></WhatAreYouCraving>
     </main>
   );
 }
