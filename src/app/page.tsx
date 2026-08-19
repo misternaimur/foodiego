d2ec26c3082b6c1f7941a355deb405492a6b2347
Anamul_Haque <anamulhaque0357@gmail.com>
2026-08-19 18:37:28 +0600

    Hero Section Created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Hero from "@/components/Hero";
 import WhatAreYouCraving from "@/components/WhatAreYouCraving";
 
 export default function Home() {
   return (
     <main className="flex-1">
+      <Hero></Hero>
       <WhatAreYouCraving></WhatAreYouCraving>
     </main>
   );
