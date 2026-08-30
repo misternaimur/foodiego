6383e82421a32795d8da31ec56079270d02c5d66
Md Naimur Rahman <jawadnaimur@gmail.com>
2026-08-30 16:27:09 +0600

    feat: add layout and cart page components for public section
    - Implemented RootLayout component for consistent layout structure.
    - Created CartPage component to manage and display cart items, including subtotal and delivery fee calculations.
    - Added loading state for cart page and empty cart message with navigation link.
    - Integrated quantity control for cart items with add/remove functionality.
    - Included order summary section with total amount and checkout button.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,23 @@ import Hero from "@/components/Hero";
 import HowItWorksSection from "@/components/HowItWorks";
 import PickedForYouSection from "@/components/PickedForYouSection";
 import WhatAreYouCraving from "@/components/WhatAreYouCraving";
+import { Navbar } from "@/components/Share/Navbar";
+import Footer from "@/components/Share/Footer";
+import { getOptionalSession } from "@/lib/dal";
 import FAQSection from "@/components/FAQ";
+
+const session = await getOptionalSession();
+
 export default function Home() {
   return (
     <main className="flex-1 bg-amber-50 " suppressHydrationWarning>
+     <Navbar user={session ? { name: session.name } : null} />
       <Hero />
       <WhatAreYouCraving />
       <PickedForYouSection />
       <FAQSection></FAQSection>
       <HowItWorksSection></HowItWorksSection>
+      <Footer></Footer>
     </main>
   );
 }
