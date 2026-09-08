cdd4949359f6264d881bce5be66d9441debe249a
Md Naimur Rahman <jawadnaimur@gmail.com>
2026-09-08 21:36:28 +0600

    make offer page

--- src/components/Share/Navbar.tsx
+++ src/components/Share/Navbar.tsx
@@ -28,7 +28,7 @@ export interface NavbarProps {
 
 const defaultNavItems: NavItem[] = [
   { label: 'Home', href: '/' },
-  { label: 'Discover Foods', href: '/foods' },
+  { label: 'Discover restaurants', href: '/restaurants' },
   { label: 'Offers', href: '/offers' },
 ];
 
