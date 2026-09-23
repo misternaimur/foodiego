1e43759b689eaa0abe3b79a750503be49569e9ad
Morshed Alam Tonmoy Bhuiya <morshed0173@gmail.com>
2026-09-23 23:43:21 +0600

    Loader updated
    add Loader for all reloade page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,14 +10,13 @@ import FAQSection from "@/components/FAQ";
 import Footer from "@/components/Share/Footer";
 import AIAssistantWidget from "@/components/AIAssistantWidget";
 import { getOptionalSession } from "@/lib/dal";
-
-
+import AppLoader from "@/components/AppLoader";
 
 
 export default async function Home() {
   const session = await getOptionalSession();
 
-  return (
+  return ( <AppLoader>
     <main className="flex-1 bg-[#FAF7EE]" suppressHydrationWarning>
       <Navbar user={session ? { name: session.name, role: session.role } : null} />
       <Hero />
@@ -31,5 +30,6 @@ export default async function Home() {
       <Footer />
       <AIAssistantWidget />
     </main>
+  </AppLoader>
   );
 }
